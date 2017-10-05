1cb1724eb26ff6eae474ccf13824fb1232b67686
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-10-05 18:07:51 +0200

    add reset interface

--- src/components/reset-buttons/reset-buttons.ts
+++ src/components/reset-buttons/reset-buttons.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+import { Reset } from '../../interfaces/reset';
 
 @Component({
     selector: 'reset-buttons',
     templateUrl: './reset-buttons.html'
 })
 
-export class ResetButtonsComponent{}
+export class ResetButtonsComponent{
+
+}
